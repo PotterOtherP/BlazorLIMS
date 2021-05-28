8037e55f0edcb0663d5d3710e6348a61b36e9888
Nate Tryon <natetryon@gmail.com>
2021-05-28 06:08:53 -0400

    Values and menus in creation form

---
+++ Data/Info.cs
@@ -0,0 +1,36 @@
+namespace BlazorLIMS.Data {
+
+    public class Info {
+
+        public static readonly string[] PlantCodes = {
+
+            "Apple",
+            "Broccoli",
+            "Cucumber",
+            "Daffodil",
+            "Elm",
+            "Fir, Fraser",
+            "Geranium",
+            "Hay",
+            "Iris",
+            "Juniper",
+            "Kale",
+            "Lettuce",
+            "Maple",
+            "Nettle",
+            "Okra",
+            "Peach",
+            "Quinoa",
+            "Raspberry",
+            "Soybean",
+            "Tobacco",
+            "Ugli",
+            "Vinifera",
+            "Wheat",
+            "Xantham",
+            "Yellow squash",
+            "Zucchini"
+        };
+
+    }
+}
