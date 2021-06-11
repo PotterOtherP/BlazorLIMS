580a3df6010148c13633ac150907154c9f01ee9d
Nate Tryon <natetryon@gmail.com>
2021-06-11 05:21:16 -0400

    Shaping up TestInfo table

--- Data/LabTestData.cs
+++ Data/LabTestData.cs
@@ -4,13 +4,13 @@ namespace BlazorLIMS.Data {
 
         public int Id { get; set; }
 
-        public string Name { get; set; }
         public string SampleCode { get; set; }
+        public string Name { get; set; }
         public bool DefaultAssigned { get; set; }
 
-        public string Units { get; set; }
         public double LowValue { get; set; }
         public double HighValue { get; set; }
+        public string Units { get; set; }
 
     }
 }
