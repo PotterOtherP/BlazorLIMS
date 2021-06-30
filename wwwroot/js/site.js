69c5c8a35e1641e265ca07640db1bec3317b871d
Nate Tryon <natetryon@gmail.com>
2021-06-30 08:42:51 -0400

    Test edit menu

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,3 +6,17 @@ function boxChecked(boxId) {
 function sayHello() {
     console.log("Hello from our JS file!");
 }
+
+function toggleVisibility(name, value) {
+
+    var element = document.getElementById(name);
+
+    if (!value)
+    {
+        element.setAttribute("style", "display: grid; grid-template-columns: 1fr 1fr 1fr; ");
+    }
+    else
+    {
+        element.setAttribute("style", "display: none; ");
+    }
+}
